8b83fe15efb5d5ca5708810b910a3fa99196747d
stancue <stancue114@gmail.com>
2026-07-17 10:54:05 +0300

    Create radaar-daemon.sh for persistent execution
    Add a script to run indefinitely while keeping the command line visible.

---
+++ levels/m06/radaar-daemon.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Ruleaza permanent ca m07. NU foloseste exec, ca sa ramana vizibila
+# linia de comanda originala (cu tot cu argumentul --sync-code=...) in ps.
+while true; do sleep 3600; done
